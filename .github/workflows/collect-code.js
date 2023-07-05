ce26b2fb47e2e9baeeca49f2cb5f1fdf0319beb4
Cihat KOÇAK <72402632+CihatKOCAK@users.noreply.github.com>
2023-07-05 23:50:53 +0300

    Update collect-code.js
    updatedForProfile

--- .github/workflows/collect-code.js
+++ .github/workflows/collect-code.js
@@ -1,33 +1,34 @@
-const fs = require("fs");
-const { Octokit } = require("@octokit/rest");
+const fs = require('fs');
+const { Octokit } = require('@octokit/rest');
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
+const octokit = new Octokit();
 
-const organization = "MainProjectsB";
+const username = 'CihatKOCAK';
 
 async function collectCode() {
   let totalLinesOfCode = 0;
 
-  const repos = await octokit.repos.listForOrg({
-    org: organization,
+  const { data: repositories } = await octokit.repos.listForUser({
+    username: username,
   });
 
-  for (const repo of repos.data) {
+  for (const repo of repositories) {
     console.log(`Collecting code from ${repo.name}`);
 
-    const contents = await octokit.repos.getContent({
-      owner: organization,
+    const { data: contents } = await octokit.repos.getContent({
+      owner: username,
       repo: repo.name,
-      path: "",
+      path: '',
     });
 
-    for (const file of contents.data) {
-      if (file.type === "file" && isCodeFile(file.name)) {
-        const downloadUrl = file.download_url;
-        const fileContent = await octokit.request(downloadUrl);
-        const linesOfCode = fileContent.data.split("\n").length;
+    for (const file of contents) {
+      if (file.type === 'file' && isCodeFile(file.name)) {
+        const { data: fileContent } = await octokit.repos.getContent({
+          owner: username,
+          repo: repo.name,
+          path: file.path,
+        });
+        const linesOfCode = countLinesOfCode(fileContent);
         totalLinesOfCode += linesOfCode;
       }
     }
@@ -39,7 +40,7 @@ async function collectCode() {
 }
 
 function isCodeFile(fileName) {
-  const extensions = [
+ const extensions = [
     ".js",
     ".py",
     ".java",
@@ -51,28 +52,41 @@ function isCodeFile(fileName) {
     ".ts",
     ".tsx",
   ];
-  const fileExtension = fileName.substring(fileName.lastIndexOf("."));
+  const fileExtension = fileName.substring(fileName.lastIndexOf('.'));
   return extensions.includes(fileExtension);
 }
 
+function countLinesOfCode(fileContent) {
+  const lines = fileContent.split('\n');
+  let linesOfCode = 0;
+
+  for (const line of lines) {
+    if (line.trim() !== '' && !line.trim().startsWith('//') && !line.trim().startsWith('/*')) {
+      linesOfCode++;
+    }
+  }
+
+  return linesOfCode;
+}
+
 function updateReadme(linesOfCode) {
-  const readmeFilePath = "README.md";
-  fs.readFile(readmeFilePath, "utf-8", (err, data) => {
+  const readmeFilePath = 'README.md';
+  fs.readFile(readmeFilePath, 'utf-8', (err, data) => {
     if (err) throw err;
 
     const updatedData = data.replace(
-      /Bu organizasyonda yer alan tüm projeler toplamda [\d,]+ satır koda sahiptir\. Giderek büyüyoruz\./,
-      `Bu organizasyonda yer alan tüm projeler toplamda ${linesOfCode.toLocaleString()} satır koda sahiptir. Giderek büyüyoruz.`
+      /This profile contains a total of [\d,]+ lines of code across all projects\. We're growing steadily\./,
+      `This profile contains a total of ${linesOfCode.toLocaleString()} lines of code across all projects. We're growing steadily.`
     );
 
-    fs.writeFile(readmeFilePath, updatedData, "utf-8", (err) => {
+    fs.writeFile(readmeFilePath, updatedData, 'utf-8', (err) => {
       if (err) throw err;
-      console.log("README updated with code statistics");
+      console.log('README updated with code statistics');
     });
   });
 }
 
 collectCode().catch((error) => {
-  console.error("Error collecting code:", error);
+  console.error('Error collecting code:', error);
   process.exit(1);
 });
